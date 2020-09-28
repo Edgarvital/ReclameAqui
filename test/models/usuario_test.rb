160596b5799178ef3e316e4c599d67fe341abc09
Edgarvital <edgar.tecnico@hotmail.com>
2020-09-28 18:40:03 -0300

    Minitest o login dos usuários deve ser unico

--- test/models/usuario_test.rb
+++ test/models/usuario_test.rb
@@ -1,9 +1,16 @@
 require 'test_helper'
 
 class UsuarioTest < ActiveSupport::TestCase
-  test 'criar um usuario teste' do
+  test 'criar um usuario valido teste' do
     @usuario = Usuario.new(login:'2424', password_digest:'password', nome:'Edgar')
     assert @usuario.save
+  end
+
+  test 'O login de um usuário deve ser unico' do
+    @usuario1 = Usuario.new(login:'2424', password_digest:'password', nome:'Edgar')
+    @usuario2 = Usuario.new(login:'2424', password_digest:'password', nome:'Edgar')
+    @usuario1.save
+    assert_not @usuario2.sav
   end
 
 end
