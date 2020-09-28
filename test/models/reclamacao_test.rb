f2f6ec589386c0a2d0b75add395de3fcee56c15e
Edgarvital <edgar.tecnico@hotmail.com>
2020-09-28 18:51:05 -0300

    Minitest Criando uma reclamacao valida

--- test/models/reclamacao_test.rb
+++ test/models/reclamacao_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class ReclamacaoTest < ActiveSupport::TestCase
-  test "the truth" do
-     assert true
+  test "Criar uma reclamacao valida teste" do
+    @reclamacao = Reclamacao.new(titulo:"A empresa1 não responde o contato", texto:"A empresa1 não responde o contato a meses", categoria:"suporte", empresa:"empresa1")
+    @usuario = Usuario.new(login:'2424', password_digest:'password', nome:'Edgar')
+    assert @usuario.save
    end
 end
--- test/models/usuario_test.rb
+++ test/models/usuario_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UsuarioTest < ActiveSupport::TestCase
-  test 'criar um usuario valido teste' do
+  test 'Criar um usuario valido teste' do
     @usuario = Usuario.new(login:'2424', password_digest:'password', nome:'Edgar')
     assert @usuario.save
   end
