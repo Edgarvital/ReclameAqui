769c4dc214f141924a1b79d21b43864a407698ba
Edgarvital <edgar.tecnico@hotmail.com>
2020-09-28 19:04:16 -0300

    Minitest o texto de uma reclamacao deve ter no minimo 10 caracteres

--- test/models/reclamacao_test.rb
+++ test/models/reclamacao_test.rb
@@ -4,6 +4,14 @@ class ReclamacaoTest < ActiveSupport::TestCase
   test "Criar uma reclamacao valida teste" do
     @reclamacao = Reclamacao.new(titulo:"A empresa1 não responde o contato", texto:"A empresa1 não responde o contato a meses", categoria:"suporte", empresa:"empresa1")
     @usuario = Usuario.new(login:'2424', password_digest:'password', nome:'Edgar')
+    @usuario.reclamacaos << @reclamacao
     assert @usuario.save
-   end
+  end
+
+  test "O texto de uma reclamacao deve ter no minimo 10 caracteres" do
+    @reclamacao = Reclamacao.new(titulo:"A empresa1 não responde o contato", texto:"A", categoria:"suporte", empresa:"empresa1")
+    @usuario = Usuario.new(login:'2425', password_digest:'password', nome:'Edgar')
+    @usuario.reclamacaos << @reclamacao
+    assert_not @usuario.save
+  end
 end
